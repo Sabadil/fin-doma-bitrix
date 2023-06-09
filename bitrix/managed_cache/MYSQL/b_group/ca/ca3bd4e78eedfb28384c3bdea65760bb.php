a2c1107f1e4672cea4dbf3f3a56a7afcdb2e5610
Ivan Zautorin <zautorin@gmail.com>
2023-06-09 11:08:29 +0300

    logs

--- bitrix/managed_cache/MYSQL/b_group/ca/ca3bd4e78eedfb28384c3bdea65760bb.php
+++ bitrix/managed_cache/MYSQL/b_group/ca/ca3bd4e78eedfb28384c3bdea65760bb.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001685717120';
-$dateexpire = '001685720720';
+$datecreate = '001686298020';
+$dateexpire = '001686301620';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:2:{s:8:"GROUP_ID";s:1:"2";s:15:"SECURITY_POLICY";N;}}';
 return true;
 ?>
